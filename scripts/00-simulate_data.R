d204bebca72f712f4109f17d9778eaca3a4a8d44
Kevin Roe <kevinroe0124@gmail.com>
2024-09-18 14:07:27 -0400

    added folder directories with placeholders

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,8 @@
+## PREAMBLE ##
+# Purpose: [UPDATE]
+# Author: Kevin Roe 
+# Date: 18 September 2024
+# Contact: kevin.roe@mail.utoronto.ca 
+# License: [... UPDATE THIS ...]
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
